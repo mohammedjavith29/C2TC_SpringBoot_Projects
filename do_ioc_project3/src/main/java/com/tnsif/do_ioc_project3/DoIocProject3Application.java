05927dbd24ac5768d00773f81cc07c34e74e61dc
lenovo <lenovo@MR-JAVITH>
2025-11-06 14:57:10 +0530

    completed

---
+++ do_ioc_project3/src/main/java/com/tnsif/do_ioc_project3/DoIocProject3Application.java
@@ -0,0 +1,13 @@
+package com.tnsif.do_ioc_project3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoIocProject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoIocProject3Application.class, args);
+	}
+
+}
