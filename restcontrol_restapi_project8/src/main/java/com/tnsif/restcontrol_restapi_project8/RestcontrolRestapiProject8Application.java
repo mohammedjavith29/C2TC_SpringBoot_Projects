9499d5d26d1a168927faad1098aa0315e3ba0419
lenovo <lenovo@MR-JAVITH>
2025-11-06 14:58:07 +0530

    ccompleted

---
+++ restcontrol_restapi_project8/src/main/java/com/tnsif/restcontrol_restapi_project8/RestcontrolRestapiProject8Application.java
@@ -0,0 +1,13 @@
+package com.tnsif.restcontrol_restapi_project8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcontrolRestapiProject8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestcontrolRestapiProject8Application.class, args);
+	}
+
+}
