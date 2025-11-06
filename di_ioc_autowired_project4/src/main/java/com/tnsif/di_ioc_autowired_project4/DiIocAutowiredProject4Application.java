b62635c6fc5d518d6654deddffc317c208575715
lenovo <lenovo@MR-JAVITH>
2025-11-06 14:54:33 +0530

    completed

---
+++ di_ioc_autowired_project4/src/main/java/com/tnsif/di_ioc_autowired_project4/DiIocAutowiredProject4Application.java
@@ -0,0 +1,13 @@
+package com.tnsif.di_ioc_autowired_project4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiIocAutowiredProject4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiIocAutowiredProject4Application.class, args);
+	}
+
+}
