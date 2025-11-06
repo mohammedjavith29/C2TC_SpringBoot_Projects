04156c322277103723cc69cf088e0633f98f416d
lenovo <lenovo@MR-JAVITH>
2025-11-06 14:50:38 +0530

    cudtomer service spring boot project completed

---
+++ customerservice/src/main/java/com/tnsif/Spring_project_1/CustomerRepository.java
@@ -0,0 +1,8 @@
+
+package com.tnsif.Spring_project_1;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+}
---
+++ customerservice/src/main/java/com/tnsif/Spring_project_1/CustomerService.java
@@ -0,0 +1,45 @@
+
+package com.tnsif.Spring_project_1;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class CustomerService {
+	
+	@Autowired
+	private CustomerRepository repo;
+	
+	//to get all the records from table
+	public List<Customer> listAll(){
+		return repo.findAll();
+		
+	}
+	
+	//save the record
+	public void save(Customer c) {
+		repo.save(c);
+	}
+	
+	
+	//to call a specific record 
+	public Customer get(Integer cid) { 
+		return repo.findById(cid).get(); 
+	}
+	
+	//delete particular record with id
+	public void delete(Integer cid) {
+		repo.deleteById(cid);
+	}
+	
+	//for update
+	public void update(Customer c) {
+		repo.save(c);
+	}
+
+}
