8f9530fa51c93c24c5ccb5ff0e472cd0e966c9df
lenovo <lenovo@MR-JAVITH>
2025-11-06 14:56:42 +0530

    completed

---
+++ di_ioc_scope_project5/src/main/java/com/tnsif/di_ioc_scope_project5/DiIocScopeProject5Application.java
@@ -0,0 +1,13 @@
+package com.tnsif.di_ioc_scope_project5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiIocScopeProject5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiIocScopeProject5Application.class, args);
+	}
+
+}
